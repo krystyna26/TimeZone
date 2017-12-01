2392a04b6525ad9ab5d1d5cac9be2ebcdfa46c77
Krystyna Swider <krystynaswider@admins-MacBook-Pro-2.local>
2017-12-01 13:06:36 -0800

    aded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'US Time Zone Display:'
+  onButtonClick(event) {
+    console.log(`Click event is working, event:`, event);
+  }
+  time = new Date();
+  lastTimeZoneSelected = null;
+
+  getDateByZone(timezone) {
+    this.time = new Date();
+    this.lastTimeZoneSelected = timezone;
+    if (timezone === 'MST') {
+      this.time.setHours(this.time.getHours() + 1);
+    } else if (timezone === 'CST') {
+      this.time.setHours(this.time.getHours() + 2);
+    } else if (timezone === 'EST') {
+      this.time.setHours(this.time.getHours() + 3);
+    } else if(timezone === "Clear") {
+      this.time = null;
+      this.lastTimeZoneSelected = null;
+    }
+    console.log(timezone, "button was selected");
+    this.lastTimeZoneSelected = timezone;
+  }
 }
